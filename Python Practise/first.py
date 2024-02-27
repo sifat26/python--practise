b27d87626c5b7798b6ca9f7fd731c41f71875fab
sifat26 <sifatict26@gmail.com>
2024-02-27 12:22:13 +0600

    first commit

---
+++ Python Practise/first.py
@@ -0,0 +1,13 @@
+print("First Program of Python")
+print("Hellow Halibut")
+age=25;
+name="Sifat";
+verify=True;
+print(age)
+print(name)
+print(verify)
+# name=input("What is Your Name? ");
+# print("Hellow "+name);
+number=input("birth");
+n=int(number);
+print(n)
