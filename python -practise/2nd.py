638ef81f5822bc72f3f5b880c9c54ff51766e169
sifat26 <sifatict26@gmail.com>
2024-03-05 12:08:26 +0600

    calculator

--- python -practise/2nd.py
+++ python -practise/2nd.py
@@ -1,6 +1,6 @@
 num_1 = input("First Number ")
-number_1=int(num_1);
+number_1=float(num_1);
 num_2 = input("Second Number ")
-number_2=int(num_2);
+number_2=float(num_2);
 sum=number_1+number_2
 print("Sum is : ",sum)
