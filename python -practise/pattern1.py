d9641b4860e9eadbd28476fff8a8705f1139b29a
sifat26 <sifatict26@gmail.com>
2024-04-24 00:25:52 +0600

    pattern1 added

---
+++ python -practise/pattern1.py
@@ -0,0 +1,10 @@
+r=5
+for i in range(0,r):
+    for j in range(0,i+1):
+        print("*",end=" ")
+    print("\r")
+for i in range(r,0,-1):
+    for j in range(0,i-1):
+        print("*",end=" ")
+    print("\r")
+
